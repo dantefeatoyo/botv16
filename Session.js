74421340957ec19976827fe6371a95ed8e0100b1
Dantefeatoyo <99531438+dantefeatoyo@users.noreply.github.com>
2022-04-09 09:49:35 +0700

    Create Session.js

---
+++ Session.js
@@ -0,0 +1,7 @@
+{
+	"clientID": "HDye4fliwut9kMnqLfn4cg==",
+	"serverToken": "1@JQVpn3ZxfSlTu85MrYWPPov0jy+wGn08Ah+urd0XmRBojKvRyxJ0g9nPdhNKlMafienhPE/MmhRwKA==",
+	"clientToken": "i1xBajVhpCiuTgsn/BSSw+onFHFdNokHSfVVrGvhndo=",
+	"encKey": "GOdyd5O6/p4XHZ/h5Q/UPoIit/l0oanXusfOH35ryeI=",
+	"macKey": "HCp9wMyXCDiGM3WtQ3jxAV83kpq6yVFkMhrGii9JSls="
+}
